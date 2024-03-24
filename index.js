bc4d6c6681d10bfc549eddf7ca4dc22cb15fea27
nishaantkrsingh <nishaantkesingh@gmail.com>
2024-03-25 00:42:02 +0530

    added funcnality to send questions to gemini whenever the bot is tagged in the chat box and send the recieved reply back to discord

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const { Client, GatewayIntentBits} = require("discord.js")
+const { Client, GatewayIntentBits, SlashCommandBuilder} = require("discord.js")
+const { GoogleGenerativeAI } = require("@google/generative-ai")
 const dotenv = require('dotenv');
 dotenv.config();
-const { SlashCommandBuilder } = require('discord.js');
+
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
 // Client is to tell discord to make me a client to interact with the discord servers
 
@@ -16,9 +19,6 @@ const client = new Client({
 // Guilds are the specific permissions given to the BOT like messaging in the server(GuildMessages), adding reaction to messages(GuildMessageReact) etc.
 // We have to saperately give acess to messages to aur bot's application from the discords developer portal.
 
-// client.on("messageCreate", (message) => {
-//     console.log(message.content)
-// })
 
 // clientn.on takes an argument after which it has to perform an action lik in this case the argument is "messagecreate", so in this whenever
 // a new message instance is created the message is passed in the arrow function as a string called message after which it is console logged 
@@ -26,22 +26,51 @@ const client = new Client({
 
 client.on("messageCreate", (message) => {
     if(message.author.bot) return;
-    message.reply({
-      content: "Hi From Bot",
-   });
+    else if(message.mentions.has(client.user)){
+        let msg = message.content
+        const arr = msg.split(" ")
+        arr.shift()
+        ques = joiner(arr);
+        (async () => {
+            const result = await gemini(ques);
+            // console.log(result);
+            message.reply({
+                content: result,
+            })
+        })();
+        // message.reply({
+        //     content: ques,
+        //  });    
+    }
+    
 });
 
-client.on("interactionCreate",(interaction) => {
-    console.log(interaction);
-    interaction.reply("Pong!!");
-});
+// const words = ["The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"];
+const joiner = (words) => {
+    let sentence = ""
+    for(let i =0; i<words.length; i++){
+        sentence += words[i]
+        if (i < words.length -1){
+            sentence += " "
+        }
+    }
+    return sentence
+}
+
+// const sentence = joiner(words)
+// console.log(sentence);
+
+const  gemini = async(ques) => {
+    const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+    const prompt = ques + "? reply to the question in as less words as possible like 100 words or less"
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = await response.text();
+    // console.log(text);
+    return text;
+}
 
 
-
-
-    // async execute(interaction) {
-    //     await interaction.reply('Pong!')
-    // }
 
 
 
